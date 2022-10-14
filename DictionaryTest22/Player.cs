e67fa7a6d7804536b3d8046e4d3b8255053ea905
Poul Henriksen <pohe@easj.dk>
2022-10-14 10:12:33 +0200

    Add project files.

---
+++ DictionaryTest22/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTest22
+{
+
+    
+    public enum PlayerType {Warrior, Wizard, Hunter}
+    public class Player
+    {
+				private int _no;
+
+		public int No
+		{
+			get { return _no; }
+			set { _no = value; }
+		}
+
+
+		public PlayerType PType { get; set; }
+
+		public string Name { get; set; }
+
+		private int _hitpoints;
+
+		public int Hitpoint
+		{
+			get { return _hitpoints; }
+			set { _hitpoints = value; }
+		}
+
+		public Player(int no, string name, PlayerType pType)
+		{
+			_no = no;
+			Name= name;
+			PType = pType;
+			if(pType ==  PlayerType.Warrior)
+			{
+				_hitpoints = 100;
+			}
+			else if (pType == PlayerType.Wizard)
+			{
+				_hitpoints = 60; 
+			} else
+			{
+				_hitpoints = 80;
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{_no} {Name} {_hitpoints}  {PType}";
+		}
+
+	}
+}
---
+++ DictionaryTest22/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DictionaryTest22;
+
+
+Player p1 = new Player(10, "Fnuggi", PlayerType.Wizard);
+Player p2 = new Player(1, "Buggi", PlayerType.Wizard);
+Player p3 = new Player(2, "Luggi", PlayerType.Warrior);
+Player p4 = new Player(17, "Hulli", PlayerType.Hunter);
+Player p5 = new Player(16, "Goggi", PlayerType.Warrior);
+
+//Console.WriteLine(p1.ToString() );
+
+
+//Player[] playerArray = new Player[5];
+//playerArray[0] = p1;
+//playerArray[1] = p2;
+//playerArray[2] = p3;
+//playerArray[3] = p4;
+//playerArray[4] = p5;
+
+//foreach (Player p in playerArray)
+//{
+//    Console.WriteLine(p.ToString());
+//}
+
+List<Player> playerList = new List<Player>() {p1, p2, p3, p4, p5};
+
+//foreach (Player item in playerList)
+//{
+//    Console.WriteLine(item);
+//}
+
+playerList.Remove(p3);
+int i = 0;
+
+while( i< playerList.Count)
+{
+    Console.WriteLine(playerList[i].ToString());
+    i++;
+}
+
+bool p3Findes = playerList.Contains(p3);
+Console.WriteLine(p3Findes);
+
+
